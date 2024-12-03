fe3cc3c4f440c9b2a77f7f501995fbbf8c5e5bdc
MSI\pelek <p.kochanski@infinity-group.pl>
2024-12-04 00:44:48 +0100

    feat: add initial SQL dump for blog database structure and tables

---
+++ Backend/data/blog.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 03 Gru 2024, 23:50
+-- Wersja serwera: 10.4.27-MariaDB
+-- Wersja PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `comments`
+--
+-- Błąd odczytu struktury tabeli blog.comments: #1932 - Table &#039;blog.comments&#039; doesn&#039;t exist in engine
+-- Błąd odczytu danych dla tabeli blog.comments: #1064 - Something is wrong in your syntax obok &#039;FROM `blog`.`comments`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `galleries`
+--
+-- Błąd odczytu struktury tabeli blog.galleries: #1932 - Table &#039;blog.galleries&#039; doesn&#039;t exist in engine
+-- Błąd odczytu danych dla tabeli blog.galleries: #1064 - Something is wrong in your syntax obok &#039;FROM `blog`.`galleries`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `posts`
+--
+-- Błąd odczytu struktury tabeli blog.posts: #1932 - Table &#039;blog.posts&#039; doesn&#039;t exist in engine
+-- Błąd odczytu danych dla tabeli blog.posts: #1064 - Something is wrong in your syntax obok &#039;FROM `blog`.`posts`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+-- Błąd odczytu struktury tabeli blog.users: #1932 - Table &#039;blog.users&#039; doesn&#039;t exist in engine
+-- Błąd odczytu danych dla tabeli blog.users: #1064 - Something is wrong in your syntax obok &#039;FROM `blog`.`users`&#039; w linii 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
